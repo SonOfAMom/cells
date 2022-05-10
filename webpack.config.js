6a84f397e4de0e322b46c5390797ff24b28288e4
Denis Fateev <denisprof@yandex.ru>
2022-05-10 19:01:50 +0300

    eslint configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,79 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env'],
+    },
+  }];
+
+  if (isProd) {
+    loaders.push({
+      loader: 'eslint-loader',
+    });
+  }
+
+  return loaders;
+};
+
 module.exports = {
-    entry: './src/index.js',
-    mode: isProd ? 'production' : 'development',
-    output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist'),
+  entry: './src/index.js',
+  mode: isProd ? 'production' : 'development',
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        }
-    },
-    devServer: {
-        'port': 3000,
-        'hot': !isProd
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            favicon: 'src/favicon.ico',
-            minify: isProd,
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist'),
-                }
-            ],
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Extracts CSS into separate files
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
+  },
+  devServer: {
+    'port': 3000,
+    'hot': !isProd,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: 'src/favicon.ico',
+      minify: isProd,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extracts CSS into separate files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
-    },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
 };
